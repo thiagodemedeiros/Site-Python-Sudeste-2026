6f331218b2d034428bd39e2dc9c0ae36f78458ce
Thiago Medeiros <thiagotitico1@gmail.com>
2025-11-19 08:53:15 -0300

    Terminando a tela de carregamento

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "./Layout"
 import Home from "./pages/home/Home"
 import PaginaPalestrantes from "./pages/palestrantes/PaginaPalestrantes"
+import Carregamento from "./components/Carregamento"
+import { useEffect, useState } from "react"
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="" element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="paginapalestrantes" element={<PaginaPalestrantes/>}/>
-        </Route>
-      </Routes>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return (<>
+    {loading ? (
+        <Carregamento/>
+      ) : (
+          <Routes>
+            <Route path="" element={<Layout/>}>
+              <Route index element={<Home/>}/>
+              <Route path="paginapalestrantes" element={<PaginaPalestrantes/>}/>
+            </Route>
+          </Routes>
+    )}
+  </>)
 }
 
 export default App
--- src/components/Carregamento.jsx
+++ src/components/Carregamento.jsx
@@ -1,9 +1,11 @@
 import './styles/Carregamento.css'
+import logo from "/logo_pyse_1.PNG"
 
 export default function Carregamento() {
     return (<>
         <div className='Carregamento'>
-            <h1>TESTE</h1>
+            <img src={logo} alt="" />
+            <h4>Carregando...</h4>
         </div>
     </>)
 }
