5d03db4ab994ca1c7200f6bae847f33ad1124d5a
Thiago Medeiros <thiagotitico1@gmail.com>
2026-02-02 21:25:20 -0300

    Corrigindo o bat para criar o cname

--- deploy.bat
+++ deploy.bat
@@ -16,6 +16,7 @@ echo CRIANDO 404.html
 echo ============================
 cd dist
 copy index.html 404.html
+echo "https://2026.sudeste.python.org.br/" > CNAME
 
 echo.
 echo ============================
